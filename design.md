f14108a88701fb16baa4ee8b5fc1d4223429ef33
theOther-Paul <paulflorentin.pf@gmail.com>
2024-06-17 12:49:27 +0300

    Added a design outline and a sidebar

---
+++ design.md
@@ -0,0 +1,37 @@
+# Design Sheet for the application
+
+## Layout
+A 2 container app, with a sidebar and a main container that shows the data in the section looked at
+### Structure
+- the sidebar that contains main menu pages, ordered based on importance
+- the main container that displays the needed data from the page
+
+
+### Functionality
+- TBD
+
+### Features
+- mobile layout/ web layout
+- feature for correcting the missing/wrong data, by uploading a correction file
+- a section to visualize the kpis in relation to the goals and to obtain the main tables in report, without downloading the report first
+
+## KPI Caluculation
+### KPIs
+- Gender diversity for paygrades for various countries. Main grades: G34+ and G37+
+- Gender distribution in education
+- ethnic distribution per regions
+
+### Calculations
+- Aggregate the population for a specified region
+- Aggregate the population for a speciffic market, depending on the KPI looked at
+- Calculate the percentages of the females in the population looked at and retain a list of employees for the same kpi to make it reusable
+- Aggregate and calculate ethnic diversity for a certain region
+- calculate the goals and ambitions of the region and market based on the type of KPI looked at
+- calculate the population present and population movements between two relative dates
+
+
+## Report design
+- section for the raw numbers and percentages of the population targeted in the kpi
+- section for population movements (if it's the case)
+- Section for comments from the receiver of the report
+- Section for corrections
--- main.py
+++ main.py
@@ -1,19 +1,66 @@
 import flet as ft  # type: ignore
 from app_class import AppFace
-
+from pathlib import Path
 
 def main(page: ft.Page):
     page.title = "KPI Reporting v.01"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.update()
 
+    # rail
+    rail = ft.NavigationRail(
+        selected_index=0,
+        label_type=ft.NavigationRailLabelType.ALL,
+        min_width=100,
+        min_extended_width=400,
+        leading=ft.Image(
+            src=f"assets/comp_logo.jpg",
+            width=100,
+            height=100,
+            fit=ft.ImageFit.CONTAIN,
+        ),
+        group_alignment=-0.9,
+        destinations=[
+            ft.FloatingActionButton(icon=ft.icons.CREATE, text="Add"),
+            ft.NavigationRailDestination(
+                icon=ft.icons.FAVORITE_BORDER,
+                selected_icon=ft.icons.FAVORITE,
+                label="First",
+            ),
+            ft.NavigationRailDestination(
+                icon_content=ft.Icon(ft.icons.BOOKMARK_BORDER),
+                selected_icon_content=ft.Icon(ft.icons.BOOKMARK),
+                label="Second",
+            ),
+            ft.NavigationRailDestination(
+                icon=ft.icons.SETTINGS_OUTLINED,
+                selected_icon_content=ft.Icon(ft.icons.SETTINGS),
+                label_content=ft.Text("Settings"),
+            ),
+        ],
+        on_change=lambda e: print("Selected destination:", e.control.selected_index),
+    )
     # create application instance
     app = AppFace()
 
+    page.add(
+        ft.Row(
+            [
+                rail,
+                ft.VerticalDivider(width=1),
+                ft.Column(
+                    [ft.Text("Body!")],
+                    alignment=ft.MainAxisAlignment.START,
+                    expand=True,
+                ),
+            ],
+            expand=True,
+        )
+    )
 
     # add application's root control to the page
     page.add(app)
 
-
+    # update before launching
+    page.update()
 
 ft.app(main)
